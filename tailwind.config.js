55944ca4696ec54c83a9d082c6b2a2095a7ada1e
pramono <pramono6236@gmail.com>
2024-11-19 13:03:11 +0700

    style: :lipstick: install dan memakai daisyui tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
+        './resources/**/*.jsx',
         './resources/**/*.js',
-        './resources/**/*.vue',
     ],
     theme: {
         extend: {
@@ -16,5 +16,8 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [require('daisyui')],
+    daisyui: {
+        themes: ["corporate"],
+    },
 };
